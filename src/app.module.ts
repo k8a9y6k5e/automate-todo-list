664eb9a4d6db97a56b6ac10b8a6a5be311786d49
k9a8y6k5e <kaykekk8965@gmail.com>
2026-03-25 21:39:58 -0300

    feat: removing of prisma and changing to typeOrm; TypeOrm configurated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { env } from 'process';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: env.DB_HOST,
+      port: Number(env.DB_PORT),
+      username: env.DB_USER,
+      password: env.DB_PASSWORD,
+      database: env.DB_DATABASE,
+      entities: [__dirname + './database/entities'],
+      synchronize: false,
+      migrations: [__dirname + './database/migrations'],
+      migrationsRun: true,
+    }),
+  ],
 })
 export class AppModule {}
