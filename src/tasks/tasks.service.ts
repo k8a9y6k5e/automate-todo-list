6bc06161aa40d82573a01f4a624c7e8f5a00f554
k9a8y6k5e <kaykekk8965@gmail.com>
2026-04-04 17:10:08 -0300

    feat: add the service to create the task;

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,14 +1,35 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { IInformation, IReturnTaskCreate, ITaskBody } from './tasks.interface';
+import {
+  ICreateTask,
+  IInformation,
+  IReturnTaskCreate,
+  ITaskBody,
+} from './tasks.interface';
 import { AiService } from '../AI/AI.service';
+import { TasksRepository } from './tasks.repository';
 
 @Injectable()
 export class TasksService {
-  constructor(private readonly aiService: AiService) {}
+  constructor(
+    private readonly aiService: AiService,
+    private readonly tasksRepository: TasksRepository,
+  ) {}
   async create(body: ITaskBody): Promise<IReturnTaskCreate> {
     for (const value of Object.values(body))
       if (!value) throw new BadRequestException('Null value entred');
 
     const informations: IInformation = await this.aiService.classificate(body);
+
+    const data: ICreateTask = {
+      name: body.name,
+      description: body.description,
+      userIdId: body.userId,
+      importance: informations.importance,
+      category: informations.category,
+    };
+
+    const id = await this.tasksRepository.insert(data);
+
+    return { id: id };
   }
 }
