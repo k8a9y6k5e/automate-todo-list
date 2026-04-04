411195ae529ff7e22bb5ec3c2637e6f8004b7ffe
k9a8y6k5e <kaykekk8965@gmail.com>
2026-04-04 17:14:56 -0300

    feat: add controller to create the value;

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { IReturnTaskCreate } from './tasks.interface';
+import { TasksService } from './tasks.service';
+import { TasksCreateDto } from './dto/create-tasks.dto';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
   @Post()
-  async create(@Body() body): Promise<IReturnTaskCreate> {}
+  async create(@Body() body: TasksCreateDto): Promise<IReturnTaskCreate> {
+    return await this.tasksService.create(body);
+  }
 }
